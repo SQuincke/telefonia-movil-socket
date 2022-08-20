f263c4b5cc78533b3e0c4cccda798418d710981f
Santiago Quincke <saquincke@gmail.com>
2022-08-20 12:33:40 -0300

    server code

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 8000
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print(f"Listening on port {PORT}")
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                message = f"Greetings from {HOST}:{PORT}\nYour message was {data.decode()}"
+                conn.sendall(message.encode())
+
+
+if __name__ == "__main__":
+    main()
